264b95fe331bcc7611628062e1cb252972cd89dd
stefanozuffi <stefano.zuffi06@outlook.com>
2026-02-24 17:24:19 +0100

    step 2

--- src/main/java/org/lessons/java/spring_mypizza/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_mypizza/controller/PizzaController.java
@@ -5,9 +5,11 @@ import org.lessons.java.spring_mypizza.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import java.util.List;
+import java.util.Optional;
    
 
 @Controller                     
@@ -23,5 +25,17 @@ public class PizzaController {
         model.addAttribute("pizzas", pizzas);           
         return "pizzas/index";                          
     }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Optional<Pizza> pizza = pizzaRepository.findById(id);
+    
+        if (pizza.isPresent()) {
+        model.addAttribute("pizza", pizza.get());
+            return "pizzas/show";
+        } else {
+            return "redirect:/pizzas";
+        }
+}
 }
 
